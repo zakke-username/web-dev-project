dc0667bc176cfccebd2298ba9d4bd439946bb559
zakke-username <zakkezweyg@gmail.com>
2025-12-11 19:52:29 +0200

    attempted fix again

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', router);
 
-app.get('/*', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../public/index.html'));
 });
 
