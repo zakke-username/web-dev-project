4cbbcfd20ec913331af8912301247bb5380aae12
zakke-username <zakkezweyg@gmail.com>
2025-12-11 20:11:07 +0200

    add final fix

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', apiRouter);
 
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile('index.html', { root: 'public' });
 });
 
