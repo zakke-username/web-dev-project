70d078e0aedde79de6870f68cd9ca76daf0cb0b6
zakke-username <zakkezweyg@gmail.com>
2025-12-11 19:46:32 +0200

    catchall route fix

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(process.cwd(), 'public', 'index.html'));
 });
 
