992fc1646a252d410cce3f9e370bae0121af374c
zakke-username <zakkezweyg@gmail.com>
2025-12-11 19:49:21 +0200

    attempted route fix

--- src/app.js
+++ src/app.js
@@ -3,21 +3,24 @@ import cors from 'cors';
 import router from './routes/index.js';
 import { notFoundHandler, errorHandler } from './middlewares/error.js';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(cors());
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(process.cwd(), 'public', 'index.html'));
-});
-
 app.use('/api', router);
 
-router.use(notFoundHandler);
-router.use(errorHandler);
+app.get('/*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../public/index.html'));
+});
+
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 export default app;
