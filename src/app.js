e6bacc0d64a976a69fbfe8d6f935fd7add0afafe
zakke-username <zakkezweyg@gmail.com>
2025-12-11 19:30:54 +0200

    add path fix

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import router from './routes/index.js';
 import { notFoundHandler, errorHandler } from './middlewares/error.js';
+import path from 'path';
 
 const app = express();
 
@@ -9,6 +10,10 @@ app.use(cors());
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(process.cwd(), 'public', 'index.html'));
+});
 
 app.use('/api', router);
 
